96366d5cb1fae03cc70c9b6308dc7d25cd3c449f
BD1026 <112836599+BD1026@users.noreply.github.com>
2023-11-16 12:03:14 +0800

    hw6

---
+++ Exercise_1116/231116.py
@@ -0,0 +1,20 @@
+class Square:
+    def __init__(self,side,length,width,radius):
+        self.side = side
+        self.length = length
+        self.width = width
+        self.radius = radius
+
+    def getSquareArea(self):
+        sied =input("邊長:")
+        getSquare = sied
+
+    def getRectangleArea(self):
+        
+        length=input('長:'+length)
+        width=input('/n寬:'+width)
+        area = length*width
+
+    def getCircleArea(self):
+        
+
